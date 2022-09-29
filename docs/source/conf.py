eb16abe9d07da34b86d3e7b150a1f0f6d155f8bf
Michael Wynne <88502274+michael-wynne-wsu@users.noreply.github.com>
2022-09-29 13:50:37 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Mukurtu Metadata Transformation Toolkit'
+copyright = '2022, CDSC'
+author = 'CDSC'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
